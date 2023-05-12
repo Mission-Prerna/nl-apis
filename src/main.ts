cfaee9ba8e1705bc0d876629677e1bb5ab2eee6b
Abhishek Mishra <abhishek@luezoid.com>
2023-05-12 12:48:40 +0530

    Using FastifyAdapter for better performance

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 // javascript does not know how to stringify BigInt
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -10,8 +11,11 @@ BigInt.prototype['toJSON'] = function () {
 };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();

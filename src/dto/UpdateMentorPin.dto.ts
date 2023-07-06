825ea6adba0236d8d9afcfff0de3aca7bcf90425
Abhishek Mishra <abhishek@luezoid.com>
2023-07-06 17:39:48 +0530

    Mentor pin update removed strict validations

--- src/dto/UpdateMentorPin.dto.ts
+++ src/dto/UpdateMentorPin.dto.ts
@@ -1,12 +1,7 @@
-import {
-  IsString, Matches,
-  MaxLength, MinLength,
-} from 'class-validator';
-
 export class UpdateMentorPinDto {
-  @IsString()
-  @MaxLength(4)
-  @MinLength(4)
-  @Matches('[0-9\-]+')
+  // removing these validations to allow app send the pin either in integer or string
+  // @MaxLength(4)
+  // @MinLength(4)
+  // @Matches('[0-9\-]+')
   pin!: number;
 }

aa817d682370aa0756c672992ff83772bc14220a
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-12-07 18:11:10 +0530

    fix: fix formatting and refactor changes

--- src/processors/assessment-visit-results.processor.ts
+++ src/processors/assessment-visit-results.processor.ts
@@ -1,4 +1,10 @@
-import { OnQueueActive, OnQueueCompleted, OnQueueFailed, Process, Processor } from '@nestjs/bull';
+import {
+  OnQueueActive,
+  OnQueueCompleted,
+  OnQueueFailed,
+  Process,
+  Processor,
+} from '@nestjs/bull';
 import { Job } from 'bull';
 import { Logger } from '@nestjs/common';
 import { AppService } from '../app.service';
@@ -8,20 +14,19 @@ import * as Sentry from '@sentry/node';
 @Processor(QueueEnum.AssessmentVisitResults)
 export class AssessmentVisitResultsProcessor {
   private readonly logger = new Logger(AssessmentVisitResultsProcessor.name);
-  constructor(
-    private readonly appService: AppService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Process(JobEnum.CreateAssessmentVisitResults)
   async createAssessmentVisitResults(job: Job) {
-    // @ts-ignore
     return await this.appService.createAssessmentVisitResult(job.data);
   }
 
   @OnQueueActive()
   onActive(job: Job) {
     this.logger.log(
-      `Processing job ${job.id} of type ${job.name} with data ${JSON.stringify(job.data)}...`,
+      `Processing job ${job.id} of type ${job.name} with data ${JSON.stringify(
+        job.data,
+      )}...`,
     );
   }
 
@@ -41,11 +46,9 @@ export class AssessmentVisitResultsProcessor {
     }
     Sentry.captureException(err, {
       user: {
-        id: mentorId
-      }
+        id: mentorId,
+      },
     });
-    this.logger.error(
-      `Failed job ${job.id} of type ${job.name}...`,
-    );
+    this.logger.error(`Failed job ${job.id} of type ${job.name}...`);
   }
 }

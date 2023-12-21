5b597c2866c54779346f72b17b6a3e96ceab40f0
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-12-21 13:46:54 +0530

    feat: allow re-assessments for examiners in a cyle id (#105)
    * feat: allow re-assessments for examiners in a cyle id
    
    * refactor code to extract validation function

--- src/app.service.ts
+++ src/app.service.ts
@@ -319,9 +319,63 @@ export class AppService {
     }
   }
 
+  async checkIfAssessmentIsValid(createAssessmentVisitResultData: CreateAssessmentVisitResult)
+  {
+    if(createAssessmentVisitResultData.actor_id == ActorEnum.EXAMINER)
+    {
+      const cycle_id = await this.prismaService.assessment_cycles.findFirst({
+        orderBy: {
+          id: 'desc'
+        }
+      });
+
+      if(!cycle_id) {
+        throw new BadRequestException("No cycles found");
+      }
+
+      const studentMappings = await this.prismaService.assessment_cycle_district_school_mapping.findFirstOrThrow({
+        where: {
+          udise: createAssessmentVisitResultData.udise,
+          cycle_id: cycle_id.id
+        }
+      })
+      const studentList = [...(studentMappings.class_1_students?.toString().split(',') || []), 
+        ...(studentMappings.class_2_students?.toString().split(',') || []),
+        ...(studentMappings.class_3_students?.toString().split(',') || [])];
+       
+      const assessmentStudentIDs = createAssessmentVisitResultData.results.map((x) => x.student_id);
+      const isStudentPresent = assessmentStudentIDs.every((x) => studentList.includes(x));
+      if(!isStudentPresent)
+      {
+        Sentry.captureMessage('Bad Assessment submission request - student not found in cycle', {
+          user: {
+            id: createAssessmentVisitResultData.mentor_id + '',
+          },
+          extra: {
+            submission_timestamp:
+              createAssessmentVisitResultData.submission_timestamp,
+            mentor_id: createAssessmentVisitResultData.mentor_id,
+            subject_id: createAssessmentVisitResultData.subject_id,
+            udise: createAssessmentVisitResultData.udise,
+            faultyStudentID: assessmentStudentIDs.filter((x) => !studentList.includes(x)),
+            mappedStudentIDs: studentList,
+          },
+        });
+        return false;
+      }
+      return true;
+    }
+  }
+
   async createAssessmentVisitResult(
     createAssessmentVisitResultData: CreateAssessmentVisitResult,
   ) {
+
+    // First check if for examiner the student submitted is in cycle
+    if(!await this.checkIfAssessmentIsValid(createAssessmentVisitResultData)) {
+      return {msg: "Subimssion is ignored, Bad Assessment submission request - student not found in cycle"}
+    }
+
     let uniqueStudents: Record<string, number> = {};
     let totalTimeTaken = 0;
     const submissionDate = new Date(); // we'll dump all records in the current quarter's table
@@ -329,6 +383,8 @@ export class AppService {
       submissionDate.getFullYear(),
       submissionDate.getMonth() + 1,
     ); // since getMonth() gives month's index
+
+
     try {
       // Checking if Assessment visit result already exist; if not we'll create it
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -1222,6 +1278,7 @@ export class AppService {
       from assessments a
       where a.mentor_id = ${mentor.id}
         and a.actor_id = ${ActorEnum.EXAMINER}
+        and a.is_valid = true
         and a.student_id in (
           select jsonb_array_elements_text(
                          dsm.class_1_students || dsm.class_2_students || dsm.class_3_students) as student_ids

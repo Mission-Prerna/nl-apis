5346f339b49716db7e055f26b291beb29f6442e9
Ajay-Maury <ajaymaurya6798@gmail.com>
2024-04-15 12:01:20 +0530

    fix: student assessment result for mentor and diet mentor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -89,8 +89,9 @@ export class AppController {
   @UseInterceptors(MentorInterceptor)
   async createAssessmentVisitResults(
     @Body(new ParseArrayPipe({ items: CreateAssessmentVisitResult })) body: CreateAssessmentVisitResult[],
-    @Request() { mentorId }: { mentorId: number },
-  ) {
+    @Request() { mentor }: { mentor: Mentor },
+    ) {
+    const mentorId = mentor.id as unknown as number;
     if (this.useQueues) {
       for (const dto of body) { // iterate over objects & push to queue
         dto.mentor_id = mentorId; // assign logged in mentor to dto

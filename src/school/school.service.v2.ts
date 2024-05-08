b237c2d0dc36af712858214bb43fe415ec2dc9f5
Ajay Kumar Maurya <ajaymaurya6798@gmail.com>
2024-05-08 16:27:51 +0530

    fix: nipun student count in students result summary (#222)
    * fix: nipun student count in students result summary
    
    * Query updated to work on the student latest assessments only

--- src/school/school.service.v2.ts
+++ src/school/school.service.v2.ts
@@ -247,38 +247,56 @@ export class SchoolServiceV2 extends SchoolService {
         summary: summary,
       };
     }
+
     for (const item of monthsForQuery) {
       const startTime = moment().month(item.month).year(item.year).date(1).startOf('day')
       const endTime = moment(startTime).add(1, 'months')
 
       const query = `
-      select t.grade,
-        count(t.student_id)                                        as assessed,
-        count(case when t.is_passed is true then t.student_id end) as nipun,
-        max(t.submission_timestamp)                                as updated_at
-      from (
-        select distinct on (a.student_id) student_id,
-                                         a.grade,
-                                         a.submitted_at,
-                                         a.submission_timestamp,
-                                         a.is_passed
-        from assessments a
-        where a.student_id is not null
-          and a.udise = $1
-          and a.actor_id = ${ActorEnum.TEACHER}
-          and a.student_id not in ('-1', '-2', '-3') --// we don't want anonymous students
-          and a.submitted_at between '${startTime.format('YYYY-MM-DD HH:mm:ss')}' and '${endTime.format('YYYY-MM-DD HH:mm:ss')}'
-          and a.grade = ANY($2::smallint[])
-          and a.is_valid = true
-        order by a.student_id, a.submitted_at DESC
-      ) t
-      group by grade`;
-      
+      WITH latest_assessments AS (
+        SELECT 
+            DISTINCT ON (student_id) 
+            student_id,
+            grade,
+            submitted_at,
+            submission_timestamp,        
+            COUNT(*) AS total_assessments
+        FROM assessments
+        WHERE student_id IS NOT NULL
+            AND udise = $1
+            AND actor_id = ${ActorEnum.TEACHER}
+            AND student_id NOT IN ('-1', '-2', '-3')
+            AND submitted_at BETWEEN '${startTime.format('YYYY-MM-DD HH:mm:ss')}' AND '${endTime.format('YYYY-MM-DD HH:mm:ss')}'
+            AND grade = ANY($2::smallint[])
+            AND is_valid = true
+        GROUP BY student_id, grade, submitted_at, submission_timestamp
+        ORDER BY student_id, submission_timestamp DESC
+    ),
+    failed_counts AS (
+        SELECT 
+            a.student_id
+        FROM assessments a
+        JOIN latest_assessments la ON a.student_id = la.student_id AND a.submission_timestamp = la.submission_timestamp
+        WHERE a.is_valid = true
+            AND a.is_passed = false
+        GROUP BY a.student_id
+    )
+    SELECT 
+        latest_assessments.grade,
+        COUNT(DISTINCT latest_assessments.student_id) AS assessed,
+        COUNT(DISTINCT latest_assessments.student_id) - COUNT(failed_counts.student_id) AS nipun,
+        MAX(latest_assessments.submission_timestamp) AS updated_at
+    FROM latest_assessments
+    LEFT JOIN failed_counts ON latest_assessments.student_id = failed_counts.student_id
+    GROUP BY latest_assessments.grade;    
+    `
+
       const result: Array<Record<string, number>> = await this.prismaService.$queryRawUnsafe(
-        query, 
-        udise, 
-        grades
-      );
+          query, 
+          udise, 
+          grades
+      );      
+
       for (const row of result) {
         gradeWiseSummary[row.grade.toString()].summary[moment().month(item.month).year(item.year).date(1).format('MMMM')].assessed = row.assessed;
         gradeWiseSummary[row.grade.toString()].summary[moment().month(item.month).year(item.year).date(1).format('MMMM')].successful = row.nipun;

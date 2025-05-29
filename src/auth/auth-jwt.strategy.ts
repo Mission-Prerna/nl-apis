2392fb3cac7ecd4c3af76a5f5f4446cd40e52005
Ajay-Maury <ajaymaurya6798@gmail.com>
2025-05-29 14:34:18 +0530

    AppId test

--- src/auth/auth-jwt.strategy.ts
+++ src/auth/auth-jwt.strategy.ts
@@ -1,9 +1,11 @@
-import { Inject } from '@nestjs/common';
+import { Inject, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
+    protected logger = new Logger(JwtStrategy.name);
+
   constructor(@Inject(ConfigService) configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -14,9 +16,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log('Decoded JWT:', payload);
-// console.log('Headers:', request.headers);
-console.log('ENV Application ID:', process.env.FA_APPLICATION_ID);
+    this.logger.log('Decoded JWT:', payload);
+// this.logger.log('Headers:', request.headers);
+this.logger.log('ENV Application ID:', process.env.FA_APPLICATION_ID);
 
     return {
       roles: payload.roles,

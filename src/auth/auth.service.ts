49df1cb9c1b319a1a8254ec53d3cc71452b71ab1
Ajay-Maury <ajaymaurya6798@gmail.com>
2025-06-18 11:27:00 +0530

    fix: send OTP

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,19 +22,24 @@ export class AuthService {
 
     private async callUserService(endpoint: string, req: any, method = 'GET') {
         const url = this.userServiceBaseURL + endpoint;
+        this.logger.log(`Calling User Service at ${url} with method ${method}`);
         try{
             const res = await axios(url, {method, headers: {'x-application-id': this.FAApplicationID, ...req.headers}, params: req.query, data: req.body });
+            this.logger.log(`User Service response: ${JSON.stringify(res.data)}`);
             return res.data;
         }
         catch (error)  
         {
             if (axios.isAxiosError(error)) {
                 const axiosError = error as AxiosError;
+                this.logger.error(`Error calling User Service: ${axiosError.message}`);
                 if (axiosError.response) {
                   const { status, data } = axiosError.response;
+                    this.logger.error(`User Service responded with status ${status} and data: ${JSON.stringify(data)}`);
                   throw new HttpException(data as string, status);
                 } 
             }
+            this.logger.error(`Unknown error calling User Service: ${error}`);
             this.logger.error(error);
             throw new HttpException('Unknown error', HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -42,16 +47,21 @@ export class AuthService {
 
     async sendOTP(request: Request) {
         const { phone } = request.query
+        this.logger.log(`Sending OTP to phone: ${phone}`);
         const mentor = await this.prismaService.mentor.findUnique({
             where:{
                 phone_no: phone as string
             }
         })
 
+        this.logger.log(`Mentor found: ${mentor ? mentor.id : 'Not found'}`);
+
         if(mentor && mentor.is_active == false){
             throw new HttpException("Your account is inactive. Please contact the DC in your District's BSA office for further assistance.", HttpStatus.FORBIDDEN)
         }
-        
+
+        this.logger.log(`Calling user service to send OTP`);
+
         const endpoint = `/api/sendOTP`;
         return await this.callUserService(endpoint, request);
     }
